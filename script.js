8d3f80282b69d0bf04759b34e14d66011a0325c9
Olena Hapchyna <olenahapchyna@gmail.com>
2025-10-25 22:58:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert("Hello, wolrd!");
+document.querySelectorAll("section").forEach((section) => {
+  const btn = section.querySelector("button");
+  const content = section.querySelector("div[id^='part']");
+  btn.addEventListener("click", () => {
+    content.classList.toggle("hidden");
+    btn.textContent = content.classList.contains("hidden") ? "▶" : "▼";
+  });
+});
+
+document.getElementById(
+  "snippet1"
+).innerHTML = `// This is a sample JS code snippet
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("Olena");`;
