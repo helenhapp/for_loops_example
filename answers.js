5f26bd554e31b7898fcf438185ae2f7c05f143ae
Olena Hapchyna <olenahapchyna@gmail.com>
2025-10-26 11:23:08 +0200

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,23 @@
+// ЗАВДАННЯ 1
+// 1. Створити масив numbers зі значенням [10, 20, 30, 40, 50]. Вивести масив у консоль (consol.log()).
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+// 2. Вивести у консоль третє значення масиву.
+console.log(numbers[2]);
+// 3. Замінити друге значення масиву на 100. Вивести оновлений масив у консоль.
+numbers[1] = 100;
+console.log(numbers);
+// 4. Вивести довжину масиву (length).
+console.log(numbers.length);
+
+// ЗАВДАННЯ 2
+// 1. Створити масив fruits зі значенням ["apple", "banana", "orange", "pineapple", "mango", "pear"]. Вивести масив у консоль.
+let fruits = ["apple", "banana", "orange", "pineapple", "mango", "pear"];
+console.log(fruits);
+// 2. Вивести у консоль друге значення масиву.
+console.log(fruits[1]);
+// 3. Замінити четверте значення масиву на "peach". Вивести оновлений масив у консоль.
+fruits[3] = "peach";
+console.log(fruits);
+// 4. Вивести довжину масиву.
+console.log(fruits.length);
